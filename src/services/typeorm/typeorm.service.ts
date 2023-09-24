cb645b22e12aa2f7ed19ffb0238a9d95db18780b
Neiser Custodio <neyser995p@gmail.com>
2023-09-24 00:12:58 -0500

    release code in cloud functions

--- src/services/typeorm/typeorm.service.ts
+++ src/services/typeorm/typeorm.service.ts
@@ -7,11 +7,11 @@ export class TypeormService implements TypeOrmOptionsFactory {
     createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
         return {
             type: 'mssql',
-            host: '********',
-            username: '********',
-            password: '********',
+            host: 'SQL5106.site4now.net',
+            username: 'db_a7224e_movies_admin',
+            password: 'Movies26#',
             port: 1433,
-            database: '********',
+            database: 'db_a7224e_movies',
             entities: ['dist/**/*.model.{ts,js}']
         }
     }
